0b6879a1f5f07ece14f085c2b50d7aeaf9e7c7c8
[Atakan Arıkan] <[atakanarkn1@outlook.com]>
2025-02-14 14:38:50 +0300

    header eklendi

--- src/assets/component/Home.jsx
+++ src/assets/component/Home.jsx
@@ -11,6 +11,13 @@ export default function Home() {
   return (
     <div className="container">
       <Header selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+      <div className="input-group">
+        <h5>Sort by: </h5>
+        <select name="" id="">
+          <option value="">Most Upvotes</option>
+        </select>
+        <button>+ Add Feedack</button>
+      </div>
     </div>
   )
 }
