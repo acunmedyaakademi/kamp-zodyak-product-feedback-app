67a635f42a12e4dad1f8346291a8afe940384b03
“[firstname <eekubilay@gmail.com>
2025-02-14 12:28:38 +0300

    home sayfasına başlandı

--- src/assets/component/Home.jsx
+++ src/assets/component/Home.jsx
@@ -1,5 +1,25 @@
+
+import { Data } from "../../App";
+import { useContext } from "react";
+
 export default function Home() {
-    return (
-        <h1>home</h1>
-    )
+
+const { data } = useContext(Data);
+
+  return (
+    <div className="container">
+      <ul>
+        {data.map((x, index) => (
+          <li key={index}>
+            <div className="feedback-cards">
+              <h6>{x.title}</h6>
+              <p>{x.description}</p>
+              <span>{x.category}</span>
+            </div>
+            
+          </li> 
+        ))}
+      </ul>
+    </div>
+  );
 }
