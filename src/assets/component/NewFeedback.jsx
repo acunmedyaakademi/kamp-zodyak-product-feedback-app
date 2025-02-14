a0be5e32ca6904f6a9c3fed36da56d4d5d487342
Ali <muhammet.ali.safak@gmail.com>
2025-02-14 14:59:17 +0300

    sayfa css calısması yapılmaya baslandı

--- src/assets/component/NewFeedback.jsx
+++ src/assets/component/NewFeedback.jsx
@@ -12,27 +12,32 @@ export default function NewFeedback() {
         </div>
 
         <div>
-          <form >
-            <h2>Create New Feedback</h2>
-            <h3>Feedback Title</h3>
-            <p>Add a short, descriptive headline</p>
-            <input type="text" />
-           
-           
-            <h3>Category</h3>
-            <p>Choose a category for your feedback</p>
-           
-            <select name="" id="">
-              <option value="feature"> Feature</option>
-              <option value="ui"> UI</option>
-              <option value="ux"> UX</option>
-              <option value="enhancement"> Enhancement</option>
-              <option value="bug"> Bug</option>
-            </select>
-            <h3>Feedback Detail</h3>
-            <p>Include any specific comments on what should be improved, added, etc.</p>
+          <form className="content">
 
-            <textarea  required name="" id=""></textarea>
+            <div  >
+              <h2>Create New Feedback</h2>
+              <h3>Feedback Title</h3>
+              <p>Add a short, descriptive headline</p>
+              <input type="text" />
+            </div>
+
+            <div >
+              <h3>Category</h3>
+              <p>Choose a category for your feedback</p>
+              <select name="" id="">
+                <option value="feature"> Feature</option>
+                <option value="ui"> UI</option>
+                <option value="ux"> UX</option>
+                <option value="enhancement"> Enhancement</option>
+                <option value="bug"> Bug</option>
+              </select>
+            </div>
+
+            <div >
+              <h3>Feedback Detail</h3>
+              <p>Include any specific comments on what should be improved, added, etc.</p>
+              <textarea required name="" id=""></textarea>
+            </div>
 
             <div className="feedback-footer">
               <button className="addfeedbackBtn">Add Feedback</button>
