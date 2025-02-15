e72a922e8d0176f9be3dfbdb1db8d0f8238b0cc1
[Atakan Arıkan] <[atakanarkn1@outlook.com]>
2025-02-15 13:55:01 +0300

    feedback-detail and  home pages completed

--- src/assets/component/Home.jsx
+++ src/assets/component/Home.jsx
@@ -7,6 +7,7 @@ import { Header } from './header.jsx';
 export default function Home() {
   const [selectedCategory, setSelectedCategory] = useState('All');
   const { data, setData, currentUser, setCurrentUser } = useContext(DataContext)
+  const [sortBy, setSortBy] = useState("Most Upvotes");
 
 
   function handleUpvotes(id) {
@@ -21,27 +22,48 @@ export default function Home() {
     setData([...data]);
   }
 
+  function openFeedback(feedback) {
+    window.location.hash = `#/feedback-detail/${feedback.id}`;
+  }
+
+  function handleSort(e) {
+    setSortBy(e.target.value);
+
+    const sortedData = data.sort((a, b) => {
+      if (e.target.value === 'Most Upvotes') {
+        return b.upvotes - a.upvotes; 
+      } else if (e.target.value === 'Least Upvotes') {
+        return a.upvotes - b.upvotes; 
+      }
+      return 0;
+    });
+
+    setData(sortedData);
+  }
+
+  
   return (
     <div className="home-container">
       <Header selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
       <div className="input-group">
         <h5>Sort by :</h5>
-        <select name="" id="">
-          <option value="">Most Upvotes</option>
+        <select name="" onChange={handleSort}>
+          <option value="Most Upvotes">Most Upvotes</option>
+          <option value="Least Upvotes">Least Upvotes</option>
         </select>
-        <button>+ Add Feedack</button>
+        <button onClick={() => window.location.hash = "#/new-feedback"}>+ Add Feedack</button>
       </div>
       {data?.length ? 
         <div className="feedbacks-list">
           {data?.map((x,i) => 
-          <div className="feedback-item" key={i}>
+          <div className="feedback-item"  key={i}>
             <div className='feedback-content'>
               <h3>{x.title}</h3>
               <p>{x.description}</p>
               <span className='category'>{x.category}</span>
               <div className="home-feedback-footer">
                 <span onClick={() => handleUpvotes(x.id)}><img src="/public/img/up-icon.svg" alt="" />{x.upvotes}</span>
-                <span><img src="/public/img/comments-icon.svg" alt="" />{x.comments ? (x.comments?.length + x.comments?.map(y => y.replies?.length || 0).reduce((a, b) => a + b, 0)) : 0}</span>
+                <span onClick={() => openFeedback(x)}><img src="/public/img/comments-icon.svg" alt="" />{x.comments ? (x.comments?.length + x.comments?.map(y => y.replies?.length || 0).reduce((a, b) => a + b, 0)) : 0}</span>
               </div>
             </div>
           </div>
@@ -55,7 +77,6 @@ export default function Home() {
             <p>Got a suggestion? Found a bug that needs to be squashed? We love hearing about new ideas to improve our app.</p>
             <button>+ Add Feedback</button>
           </div>
-
         </div>
       }
     </div>
