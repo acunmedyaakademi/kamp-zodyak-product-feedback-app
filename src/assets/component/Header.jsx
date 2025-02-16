8bcad3ae5f1bf972c7c3fce5376f5d1f40d21b0a
Merve Ozel <leylakmerve@gmail.com>
2025-02-16 14:44:12 +0300

    updates

--- src/assets/component/Header.jsx
+++ src/assets/component/Header.jsx
@@ -1,14 +1,13 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Data } from '../../App.jsx';
 
 
 
-export function Header({ selectedCategory, setSelectedCategory}) {
-  const {data} = useContext(Data)
+export function Header({ selectedCategory, setSelectedCategory }) {
+  const { data } = useContext(Data)
   console.log(data);
-  return(
+  return (
     <>
-      <h1>Header</h1>
     </>
   )
 }
